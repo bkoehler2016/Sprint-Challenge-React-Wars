0d89b406ec8664b9c6cbbed20b43bde90c5712dc
Ben Koehler <bkoehler85282@gmail.com>
2019-11-22 19:18:55 -0700

    got info from api

---
+++ starwars/src/components/StarCard.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { Card, CardTitle, CardText } from "reactstrap";
+
+const StarCard = props => {
+  console.log(props.character);
+  console.log(props.howTall);
+  console.log(props.props.weight);
+};
+
+export default StarCard;
--- starwars/src/index.js
+++ starwars/src/index.js
@@ -1,6 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.min.css";
+import styled from "styled-components";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
